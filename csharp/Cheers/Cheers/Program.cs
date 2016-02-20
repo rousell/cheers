fa7f8b896d318b9c89049a50d7af98aca3fb801b
rousell <lauren.l.rouse@gmail.com>
2016-02-20 11:58:58 -0600

    Part 1 All Complete

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -16,9 +16,15 @@ namespace Cheers
             name = Console.ReadLine();
             for (int i = 1; i <= name.Length; i++)
             {
-                Console.WriteLine("Give me a .. " + name[i-1]);
+                var lowerName = name.ToLower(); 
+                var vowelarray = new char[] { 'h','a','l','f','n','o','r','s','e','m','i','x'};
+                if (vowelarray.Contains(lowerName[i-1])) {
+                    Console.WriteLine("Give me an .. " + lowerName[i - 1]);
+                }  else {
+                    Console.WriteLine("Give me a .. " + lowerName[i - 1]);
+                }
             }
-            Console.WriteLine(name + " is... GRAND!");
+            Console.WriteLine(name.ToUpper() + " is... GRAND!");
             Console.Write("\nPress any KEY to close the console.");
             Console.ReadKey();
         }
